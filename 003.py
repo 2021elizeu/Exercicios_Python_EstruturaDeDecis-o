2532fd9d6775fbc4c3131ea8ebb647bb9e2a2e8a
Elizeu Figueiredo <93289473+2021elizeu@users.noreply.github.com>
2024-01-23 17:22:33 -0300

    Add files via upload

---
+++ 003.py
@@ -0,0 +1,13 @@
+"""Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido."""
+
+sexo = input('Escolha seu sexo é F para Feminino e M para Masculino:')
+
+if sexo == 'F':
+     print('Feminino')
+elif sexo == 'M':
+     print ('Masculino')
+else:
+     print('Sexo invalido')
+ 
+
+
